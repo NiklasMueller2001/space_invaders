16293006984c11aa904a62912ffd5d6edefa34b8
Niklas Mueller <n.mueller@medienagentur24.eu>
2024-01-31 21:39:01 +0100

    Add LevelGenerator
    Implemented a LevelGenerator class that creates the initial enemies at the start of a new level at each iteration and stores the current level number. At the start of each level 5 rows of 11 equidistently places enemies are created. With increasing level number the starting position of these enemies is lowered by a small offset.

---
+++ space_invaders/components/level.py
@@ -0,0 +1,39 @@
+from space_invaders.components.objects import EnemyCreator, Enemy
+from typing import Any, Iterator
+import pygame
+import yaml
+
+with open("space_invaders/config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+WIDTH = config["WIDTH"]
+HEIGHT = config["HEIGHT"]
+ENEMY_BASE_SPEED = config["ENEMY_BASE_SPEED"]
+enemy_im = pygame.image.load("space_invaders/assets/alien.gif")
+enemy_im = pygame.transform.scale(enemy_im, (0.05 * WIDTH, 0.08 * HEIGHT))
+
+
+class LevelGenerator:
+    def __init__(self, enemy_creator: EnemyCreator) -> None:
+        self.enemy_creator = enemy_creator
+        self.level_number = 0
+
+    def initial_enemies(self, y_offset) -> list[Enemy]:
+        enemies = []
+        for x in range(0, 11):
+            for y in range(0, 5):
+                new_enemy = self.enemy_creator.create_enemy(
+                    enemy_im,
+                    (
+                        x * 0.6 * WIDTH / 10 + 0.2 * WIDTH,
+                        y * enemy_im.get_rect().height + y_offset,
+                    ),
+                    ENEMY_BASE_SPEED,
+                )
+                enemies.append(new_enemy)
+        return enemies
+
+    def __next__(self) -> Iterator[list[Enemy]]:
+        self.level_number += 1
+        y_offset = 0.05 * WIDTH * (self.level_number - 1)
+        return self.initial_enemies(y_offset)
