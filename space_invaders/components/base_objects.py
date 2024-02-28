7f07a9a96bd0bf2c16ec1ac8b9d744ac185ba7cd
Niklas Mueller <n.mueller@medienagentur24.eu>
2024-02-28 20:57:40 +0100

    Fix Laser inheritance
    Made Laser inherit from MovableObject to make code
    more compact. Added the abstract method
    update(self) -> None to the MovableObect class for
    consistency.

--- space_invaders/components/base_objects.py
+++ space_invaders/components/base_objects.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Self
 import pygame
 
@@ -5,7 +6,7 @@ import pygame
 class BaseObject(pygame.sprite.Sprite):
     """Base class for all visible objects in the game."""
 
-    def __init__(self, image, initial_pos: tuple) -> None:
+    def __init__(self, image: pygame.Surface, initial_pos: tuple) -> None:
         super().__init__()
         self.image = image
         self.rect = image.get_rect(topleft=(0, 0)).move(initial_pos)
@@ -13,19 +14,27 @@ class BaseObject(pygame.sprite.Sprite):
 
     def rescale(self, size: tuple[int, int]) -> Self:
         """Method for rescaling imag of object"""
+
         self.image = pygame.transform.scale_by(self.image, size)
         return self
 
 
-class MovableObject(BaseObject):
+class MovableObject(ABC, BaseObject):
     """Base class for all movable visible objects in the game."""
 
-    def __init__(self, image, initial_pos: tuple, speed: int) -> None:
+    def __init__(self, image: pygame.Surface, initial_pos: tuple, speed: int) -> None:
         super().__init__(image, initial_pos)
         self.speed = speed
         self.rect = image.get_rect(topleft=(0, 0)).move(initial_pos)
 
     def rescale(self, size: tuple[int, int]) -> Self:
         """Method for rescaling imag of object"""
+
         self.image = pygame.transform.scale_by(self.image, size)
         return self
+
+    @abstractmethod
+    def update(self) -> None:
+        """Define the movement of the sprite."""
+
+        pass
--- space_invaders/components/laser.py
+++ space_invaders/components/laser.py
@@ -1,6 +1,7 @@
+from typing import Any
+from space_invaders.components.base_objects import MovableObject
 import pygame
 import yaml
-from typing import Any
 
 
 def get_config() -> Any:
@@ -13,16 +14,18 @@ config = get_config()
 HEIGHT = config["HEIGHT"]
 
 
-class Laser(pygame.sprite.Sprite):
+class Laser(MovableObject):
+    """
+    Class for sprites representing lasers that can be shot by enemies and the
+    player.
+    """
+
     directions = {"up": -1, "down": 1}
 
-    def __init__(self, image, initial_pos: tuple, speed: int) -> None:
-        super().__init__()
-        self.image = image
-        self.speed = speed
-        self.rect = image.get_rect(topleft=(0, 0)).move(initial_pos)
+    def __init__(self, image: pygame.Surface, initial_pos: tuple, speed: int) -> None:
+        super().__init__(image, initial_pos, speed)
 
-    def update(self, direction: str, dt):
+    def update(self, direction: str, dt) -> None:
         self.rect.centery += Laser.directions[direction] * self.speed * dt
         if self._is_out_of_screen():
             self.kill()
